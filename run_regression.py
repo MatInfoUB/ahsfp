5149c2f730c1d89c60be5c0d87aedadfd4001e4b
Arpan Mukherjee <arpanmuk@buffalo.edu>
2020-03-28 06:06:23 -0400

    minor changes

--- run_regression.py
+++ run_regression.py
@@ -3,9 +3,18 @@ import seaborn as sns
 
 X, y = mofpy.load_input_data()
 input_shape = X.shape[1:]
-model = mofpy.Regressor(input_shape=input_shape)
+model = mofpy.Regressor(input_shape=input_shape, epochs=100)
 
 model.build_model()
 model.compile()
 
-model.fit(X, y, verbose=1)
+model.fit(X, y, kfold=False, verbose=1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+y_test_pred = model.predict(X_test).reshape(len(y_test))
+import pandas as pd
+Pred_table = pd.DataFrame({'Observed': y_test, 'Predicted':
+    y_test_pred})
+sns.jointplot(x='Observed', y='Predicted', data=Pred_table)
+print('Testing R-squared is: ', model.r_2_score(X_test, y_test))
